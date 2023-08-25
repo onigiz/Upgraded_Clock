0372124e34b8cef2af6841e80fbb141e4f0affe2
Onur Nigiz <75140593+onigiz@users.noreply.github.com>
2023-08-25 19:19:59 +0300

    Update | v1.0

--- main.ino
+++ main.ino
@@ -7,16 +7,20 @@
 #define RST_PIN 8
 #define buzzer1 13
 #define buzzer2 5
+#define button 2
+#define button2 3
 int month = 0;
+int button_value = 0;
+int button_value2 = 0;
+int alarm_set = 0;
+int message_mode = 1;
 
 virtuabotixRTC myRTC(CLK_PIN, DATA_PIN, RST_PIN);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-char *months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
-int alarm_hours = 1;
-int alarm_minutes = 00;
-int alarm_type = 3;
+int alarm_hours = 0;
+int alarm_minutes = 0;
+int alarm_type = 0;
 
 byte customChar_Heart[] = {B00000, B01010, B11111, B11111, B11111, B01110, B00100, B00000};
 byte customChar_Smiley[] = {B00000, B00000, B01010, B00000, B10001, B01110, B00000, B00000};
@@ -29,10 +33,14 @@ byte customChar_u[] = {B01010, B00000, B10001, B10001, B10001, B10011, B01101, B
 
 void setup() 
 {
+  Serial.begin(9600);
+  //myRTC.setDS1302Time(15, 23, 12, 5, 25, 8, 2023);
   lcd.init();
   lcd.backlight();
   pinMode(buzzer1, OUTPUT);
   pinMode(buzzer2, OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(button2, INPUT);
 
   lcd.createChar(0, customChar_Heart);
   lcd.createChar(1, customChar_Smiley);
@@ -75,11 +83,29 @@ void alarm3(int pin1, int pin2)
 {
   digitalWrite(pin1,HIGH);
   digitalWrite(pin2,HIGH);
-  delay(100);
+  delay(500);
   digitalWrite(pin1, LOW);
   digitalWrite(pin2, LOW);
-  delay(100);
+  delay(500);
 }
+
+int alarm_type_choose(int butt)
+{
+  button_value = digitalRead(butt);
+  if(button_value == HIGH)
+  {
+    digitalWrite(buzzer1, HIGH);
+    alarm_type++;
+    if (alarm_type < 0 || alarm_type > 3)
+      alarm_type = 0;
+    delay(100);
+    digitalWrite(buzzer1, LOW);
+    
+    return (alarm_type);
+  }   
+}
+
+
 //Burada alarm_type'ı yanlışlıkla 3'ten büyük veya 1'den küçük alırsak diye kontrol yazmam lazım
 void alarm_call(int alarm_type)
 {
@@ -90,6 +116,167 @@ void alarm_call(int alarm_type)
   else if (alarm_type == 3)
     alarm3(buzzer1, buzzer2);
 }
+
+void alarm_status(int alarm_type)
+{
+  lcd.setCursor(0, 2);
+  lcd.print("Alarm:");
+  if (alarm_type <= 3 && alarm_type > 0)
+  {
+    if (alarm_hours < 10)
+      lcd.print("0");
+    lcd.print(alarm_hours);
+    lcd.print(":");
+    if(alarm_minutes < 10)
+      lcd.print("0");
+    lcd.print(alarm_minutes);
+    lcd.print("(");
+    lcd.print(alarm_type);
+    lcd.print(")");
+  }
+  else 
+    lcd.print("OFF"); 
+}
+
+void alarm_set_init_print()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.print("Set alarm: ");
+  if (alarm_hours < 10)
+    lcd.print("0");
+  lcd.print(alarm_hours);
+  lcd.print(":");
+  if (alarm_minutes < 10)
+    lcd.print("0");
+  lcd.print(alarm_minutes);
+  lcd.setCursor(0, 1);
+  alarm_type = 0;
+  lcd.print("Alarm Type: ");
+  if (alarm_type == 0)
+    lcd.print("OFF");
+}
+
+void alarm_set_func()
+{
+  alarm_set_init_print();
+  while (button_value2 == 2 || button_value2 == 3)
+  {    
+    while(button_value2 >= 2 && button_value2 < 5)
+    {
+      if (button_value2 == 2)
+      {
+        if(digitalRead(button) == HIGH)
+        {
+          alarm_hours++;
+          if (alarm_hours >= 24)
+            alarm_hours = 0;
+          lcd.setCursor(11, 0);  
+          if (alarm_hours < 10)
+            lcd.print("0");
+          lcd.print(alarm_hours);
+          delay(10);
+        }
+        if(digitalRead(button2) == HIGH)
+        {
+          digitalWrite(buzzer1, HIGH);
+          delay(100);
+          digitalWrite(buzzer1, LOW);
+          delay(50);
+          button_value2 = 3;
+        }
+      }
+
+
+      if(button_value2 == 3)
+      {
+        if(digitalRead(button) == HIGH)
+        {
+          alarm_minutes += 5;
+          if (alarm_minutes >= 60)
+            alarm_minutes = 0;
+          lcd.setCursor(14, 0);
+
+          if (alarm_minutes < 10)
+            lcd.print("0");
+          lcd.print(alarm_minutes);
+          delay(10);
+        }
+        if(digitalRead(button2) == HIGH)
+        {
+          digitalWrite(buzzer1, HIGH);
+          delay(100);
+          digitalWrite(buzzer1, LOW);
+          delay(50);
+          button_value2 = 4;
+        }
+        
+      }
+      
+      if(button_value2 == 4)
+      {
+
+        lcd.setCursor(11, 1);
+        if (digitalRead(button) == HIGH)
+        {
+          alarm_type++;
+          if (alarm_type > 3 || alarm_type <= 0)
+          {
+            alarm_type = 0;
+            lcd.print("OFF");
+          }
+          else
+          {
+            lcd.print(alarm_type);
+            lcd.print("   ");
+            int i = 0;
+            while(i < 2)
+            {
+              delay(1000);
+              alarm_call(alarm_type);
+              i++;
+            }
+          }
+          //Bu loopla senkronizasyon alarm_type döngülerinin toplam 1000ms olması lazım her zaman
+          
+        }
+
+
+        if(digitalRead(button2) == HIGH)
+        {
+          digitalWrite(buzzer1, HIGH);
+          delay(100);
+          digitalWrite(buzzer1, LOW);
+          delay(50);
+          button_value2 = 5;
+        }
+      }
+
+
+      if (button_value2 >= 5)
+      {
+        if(digitalRead(button2) == HIGH)
+        {
+          button_value2 = 6;
+          digitalWrite(buzzer1, HIGH);
+          delay(100);
+          digitalWrite(buzzer1, LOW);
+          delay(50);
+        }
+        if (button_value2 = 6)
+        {
+        digitalWrite(buzzer1, HIGH);
+        delay(250);
+        digitalWrite(buzzer1, LOW);
+        button_value = 1;
+        button_value2 = 0;
+        }
+      }
+      delay(500);  
+    }
+  }
+}
+
 /*  DATE - TIME ********************************************************************************************************************/
 void months_func(int month)
 {
@@ -177,13 +364,24 @@ void show_hour()
 void loop() 
 {
   myRTC.updateTime();
+  
   lcd.clear();
-
+  alarm_type_choose(button);
   show_date();
+  alarm_status(alarm_type);
   show_hour();
-
 
   //alarm modların toplam delay'i 1000'e eşit olmazsa saniye yazdırımında atlamalar meydana geliyor
   if (alarm_hours == myRTC.hours && alarm_minutes == myRTC.minutes)
-    alarm_call(alarm_type);  
+    alarm_call(alarm_type);
+
+  if (digitalRead(button2) == HIGH)
+    button_value2++;
+  if (button_value2 == 2)
+  {
+    digitalWrite(buzzer1, HIGH);
+    delay(250);
+    digitalWrite(buzzer1, LOW);
+    alarm_set_func();
+  }  
 }
